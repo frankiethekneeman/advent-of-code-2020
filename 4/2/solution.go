1dea38c692a6f42e5c69001975e00766e149bcfe
Francis Sirizzotti <francis@sirizzotti.info>
2020-12-02 08:36:43 -0500

    Framework for the remaining puzzles.

---
+++ 4/2/solution.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+)
+
+type RESULT_TYPE = int
+
+/*
+Begin Solution
+*/
+
+func solution(lines []string) RESULT_TYPE {
+    return -1;
+}
+
+/*
+Test Cases
+*/
+func TEST_CASES() []RESULT_TYPE {
+    return []RESULT_TYPE {
+    }
+}
+
+func checkErr(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+    DAY := 4
+    passedTests := true
+    for i, expected := range TEST_CASES(){
+        result := solution(getTest(DAY, i + 1))
+        if result != expected {
+            fmt.Printf("Test %d: expected %v, but got %v instead!\n", i+1, expected, result)
+            passedTests = false
+        }
+    }
+    if passedTests {
+        fmt.Printf("%v\n", solution(getInput(DAY)))
+    }
+}
+
+func getInput(day int) []string {
+    return getFile(strconv.Itoa(day) + "/input")
+}
+func getTest(day int, test int) []string {
+    return getFile(strconv.Itoa(day) + "/test" + strconv.Itoa(test))
+}
+
+func getFile(location string) []string {
+    file, err := os.Open(location)
+    checkErr(err)
+    scanner := bufio.NewScanner(file)
+    var lines []string
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    checkErr(scanner.Err())
+    return lines
+}
