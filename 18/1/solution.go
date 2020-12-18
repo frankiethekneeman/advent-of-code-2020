2ea71a4dbe001637de9f424a797598e152cd1483
Francis Sirizzotti <francis@sirizzotti.info>
2020-12-18 09:53:18 -0500

    Day 18, in the bag.

--- 18/1/solution.go
+++ 18/1/solution.go
@@ -4,6 +4,7 @@ import (
     "bufio"
     "fmt"
     "os"
+    "regexp"
     "strconv"
 )
 
@@ -13,8 +14,104 @@ type RESULT_TYPE = int
 Begin Solution
 */
 
+type Expr interface {
+    evaluate() int
+}
+
+type BinaryExpr struct {
+    lhs, rhs Expr
+    operator rune
+}
+
+type Term struct {
+    value int
+}
+
+func (e BinaryExpr) evaluate() int {
+    l := e.lhs.evaluate()
+    r := e.rhs.evaluate()
+    if e.operator == '*' {
+        return l * r
+    }else if e.operator == '+' {
+        return l + r
+    }
+    panic("Unrecognized operator: " + string(e.operator))
+}
+
+func (t Term) evaluate() int {
+    return t.value
+}
+
+var TOKENIZER *regexp.Regexp = regexp.MustCompile(`\d+|[*+()]`)
+
+func consumeTerm(tokens []string) (Term, []string) {
+    value, err := strconv.Atoi(tokens[0])
+    checkErr(err)
+    return Term { value }, tokens[1:]
+}
+
+func consume(expected string, tokens []string) []string {
+    if tokens [0] == expected {
+        return tokens[1:]
+    }
+    panic("Expected " + expected + " but found "+ tokens[0])
+}
+
+func consumeLHS(tokens []string) (Expr, []string) {
+    if tokens[0] == ")" {
+        tokens = consume(")", tokens)
+        // Deal with parentheses
+        lhs, tokens := consumeExpr(tokens)
+        tokens = consume("(", tokens)
+        return lhs, tokens
+    } else {
+        lhs, rest := consumeTerm(tokens)
+        return lhs, rest
+    }
+}
+
+func consumeOperator(tokens []string) (rune, []string) {
+    if tokens[0] == "*" || tokens[0] == "+" {
+        return []rune(tokens[0])[0], tokens[1:]
+    }
+    panic("Expected operator but got " + string(tokens[0]))
+}
+
+func consumeExpr(tokens []string) (Expr, []string) {
+    lhs, tokens := consumeLHS(tokens)
+    if len(tokens) == 0 || tokens[0] == "(" {
+        return lhs, tokens
+    }
+
+    operator, tokens := consumeOperator(tokens)
+    
+    rhs, tokens := consumeExpr(tokens)
+
+    //fmt.Printf("%v %c %v\n", lhs, operator, rhs)
+
+    return BinaryExpr{lhs, rhs, operator}, tokens
+}
+func parse(line string) Expr {
+    tokens := TOKENIZER.FindAllString(line, -1)
+    //fmt.Printf("%v\n", tokens)
+    for i, j := 0, len(tokens) - 1; i < j; i, j = i + 1, j - 1 {
+        tokens[i], tokens[j] = tokens[j], tokens[i]
+    }
+    //fmt.Printf("%v\n", tokens)
+    toReturn, remaining := consumeExpr(tokens)
+    if len(remaining) != 0 {
+        panic("Failed Parse")
+    }
+    return toReturn
+}
+
+
 func solution(lines []string) RESULT_TYPE {
-    return -1;
+    sum := 0
+    for _, line := range(lines) {
+        sum += parse(line).evaluate()
+    }
+    return sum;
 }
 
 /*
@@ -22,6 +119,13 @@ Test Cases
 */
 func TEST_CASES() []RESULT_TYPE {
     return []RESULT_TYPE {
+        71,
+        51,
+        26,
+        437,
+        12240,
+        13632,
+        26457,
     }
 }
 
--- 18/2/solution.go
+++ 18/2/solution.go
@@ -4,6 +4,7 @@ import (
     "bufio"
     "fmt"
     "os"
+    "regexp"
     "strconv"
 )
 
@@ -13,8 +14,98 @@ type RESULT_TYPE = int
 Begin Solution
 */
 
+type Expr interface {
+    evaluate() int
+}
+
+type BinaryExpr struct {
+    lhs, rhs Expr
+    operator rune
+}
+
+type Term struct {
+    value int
+}
+
+func (e BinaryExpr) evaluate() int {
+    l := e.lhs.evaluate()
+    r := e.rhs.evaluate()
+    if e.operator == '*' {
+        return l * r
+    }else if e.operator == '+' {
+        return l + r
+    }
+    panic("Unrecognized operator: " + string(e.operator))
+}
+
+func (t Term) evaluate() int {
+    return t.value
+}
+
+var TOKENIZER *regexp.Regexp = regexp.MustCompile(`\d+|[*+()]`)
+
+func consumeTerm(tokens []string) (Term, []string) {
+    value, err := strconv.Atoi(tokens[0])
+    checkErr(err)
+    return Term { value }, tokens[1:]
+}
+
+func consume(expected string, tokens []string) []string {
+    if tokens [0] == expected {
+        return tokens[1:]
+    }
+    panic("Expected " + expected + " but found "+ tokens[0])
+}
+
+func consumeLHS(tokens []string) (Expr, []string) {
+    if tokens[0] == "(" {
+        tokens = consume("(", tokens)
+        // Deal with parentheses
+        lhs, tokens := consumeMult(tokens)
+        tokens = consume(")", tokens)
+        return lhs, tokens
+    } else {
+        lhs, rest := consumeTerm(tokens)
+        return lhs, rest
+    }
+}
+
+func consumeAdd(tokens []string) (Expr, []string) {
+    lhs, tokens := consumeLHS(tokens)
+    if len(tokens) == 0 || tokens[0] != "+" {
+        return lhs, tokens
+    }
+    tokens = consume("+", tokens)
+    rhs, tokens := consumeAdd(tokens)
+    return BinaryExpr{lhs, rhs, '+'}, tokens
+}
+
+func consumeMult(tokens []string) (Expr, []string) {
+    lhs, tokens := consumeAdd(tokens)
+    if len(tokens) == 0 || tokens[0] != "*" {
+        return lhs, tokens
+    }
+    tokens = consume("*", tokens)
+    rhs, tokens := consumeMult(tokens)
+    return BinaryExpr{lhs, rhs, '*'}, tokens
+}
+
+func parse(line string) Expr {
+    tokens := TOKENIZER.FindAllString(line, -1)
+    toReturn, remaining := consumeMult(tokens)
+    if len(remaining) != 0 {
+        panic("Failed Parse")
+    }
+    return toReturn
+}
+
+
 func solution(lines []string) RESULT_TYPE {
-    return -1;
+    sum := 0
+    for _, line := range(lines) {
+        sum += parse(line).evaluate()
+    }
+    return sum;
 }
 
 /*
@@ -22,6 +113,13 @@ Test Cases
 */
 func TEST_CASES() []RESULT_TYPE {
     return []RESULT_TYPE {
+        231,
+        51,
+        46,
+        1445,
+        669060,
+        23340,
+        694173,
     }
 }
 
