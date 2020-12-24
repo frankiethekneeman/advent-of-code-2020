7e79c83e4999c2ef8536add31b19e00ae78df9e1
Francis Sirizzotti <francis@sirizzotti.info>
2020-12-24 09:39:18 -0500

    24

--- 24/1/solution.go
+++ 24/1/solution.go
@@ -4,6 +4,7 @@ import (
     "bufio"
     "fmt"
     "os"
+    "regexp"
     "strconv"
 )
 
@@ -13,8 +14,45 @@ type RESULT_TYPE = int
 Begin Solution
 */
 
+
 func solution(lines []string) RESULT_TYPE {
-    return -1;
+    black := make(map[Tile] bool)
+    for _, line := range lines {
+        current := Tile{}
+        for _, step := range parse(line) {
+            current = current.update(BASIS_VECTORS[step])
+        }
+        if black[current] {
+            delete(black, current)
+        } else {
+            black[current] = true
+        }
+    }
+
+    return len(black);
+}
+
+var DIRECTION_REGEX = regexp.MustCompile("(e|w|ne|nw|se|sw)")
+func parse(line string) []string {
+    return DIRECTION_REGEX.FindAllString(line, -1)
+}
+type Tile struct {x,y,z int}
+
+var BASIS_VECTORS = map[string]Tile {
+    "w": Tile{-1, 1, 0},
+    "e": Tile{1, -1, 0},
+    "se": Tile{0, -1, +1},
+    "sw": Tile{-1, 0, +1},
+    "ne": Tile{1, 0, -1},
+    "nw": Tile{0, 1, -1},
+}
+
+func (t Tile) update (o Tile) Tile {
+    return Tile{
+        t.x + o.x,
+        t.y + o.y,
+        t.z + o.z,
+    }
 }
 
 /*
@@ -22,6 +60,7 @@ Test Cases
 */
 func TEST_CASES() []RESULT_TYPE {
     return []RESULT_TYPE {
+        10,
     }
 }
 
--- 24/2/solution.go
+++ 24/2/solution.go
@@ -4,6 +4,7 @@ import (
     "bufio"
     "fmt"
     "os"
+    "regexp"
     "strconv"
 )
 
@@ -13,8 +14,76 @@ type RESULT_TYPE = int
 Begin Solution
 */
 
+const NUMSTEPS = 100
+
+
 func solution(lines []string) RESULT_TYPE {
-    return -1;
+    black := make(map[Tile] bool)
+    for _, line := range lines {
+        current := Tile{}
+        for _, step := range parse(line) {
+            current = current.update(BASIS_VECTORS[step])
+        }
+        if black[current] {
+            delete(black, current)
+        } else {
+            black[current] = true
+        }
+    }
+
+    for i := 0; i < NUMSTEPS; i++ {
+        black = next(black)
+    }
+
+    return len(black);
+}
+
+var DIRECTION_REGEX = regexp.MustCompile("(e|w|ne|nw|se|sw)")
+func parse(line string) []string {
+    return DIRECTION_REGEX.FindAllString(line, -1)
+}
+type Tile struct {x,y,z int}
+
+var BASIS_VECTORS = map[string]Tile {
+    "w": Tile{-1, 1, 0},
+    "e": Tile{1, -1, 0},
+    "se": Tile{0, -1, +1},
+    "sw": Tile{-1, 0, +1},
+    "ne": Tile{1, 0, -1},
+    "nw": Tile{0, 1, -1},
+}
+
+func (t Tile) update (o Tile) Tile {
+    return Tile{
+        t.x + o.x,
+        t.y + o.y,
+        t.z + o.z,
+    }
+}
+
+func next(prev map[Tile] bool) map[Tile] bool {
+    state := make(map[Tile] bool)
+    whiteTiles := make(map[Tile] int)
+    for blackTile := range prev {
+        activeNeighbors := 0
+        for _, delta := range BASIS_VECTORS {
+            neighbor := blackTile.update(delta)
+            if prev[neighbor] {
+                activeNeighbors++
+            } else {
+                whiteTiles[neighbor]++
+            }
+        }
+        if activeNeighbors > 0 && activeNeighbors < 3 {
+            state[blackTile] = true
+        }
+    }
+    for whiteTile, neighbors := range whiteTiles {
+        if neighbors == 2 {
+            state[whiteTile] = true
+        }
+    }
+    return state
 }
 
 /*
@@ -22,6 +91,7 @@ Test Cases
 */
 func TEST_CASES() []RESULT_TYPE {
     return []RESULT_TYPE {
+        2208,
     }
 }
 
